63c8cd1a2b610f73ba9086ff3a75a4857aea1557
Your Name <you@example.com>
2023-04-10 18:40:01 +0200

    Cambio los métodos, el anterior no era el definitivo :( y este tampoco :((((

--- danmorper.R
+++ danmorper.R
@@ -4,26 +4,27 @@ library(magrittr)
 real_dens <- function(x) {
   0.5 * dnorm(x, mean = -1.5) + 0.5 * dnorm(x, mean = 1.5)
 }
-# Creamos la rejilla de valores representativos de la v.a. Z----
-grid <- seq(grid_min, grid_max, length = grid_size)
 
 # Definimos el número de muestras y los tamaños de muestra
 M <- 1000
 ns <- c(50, 100)
-n_rejilla <- 512
+grid_min <- -4
+grid_max <- 4
+n_grid <- 512
+grid <- seq(grid_min, grid_max, length = n_grid)
 
 # Definimos los métodos I y II para elegir el ancho de ventana----
 # Ancho de banda knn y kernel gaussiano
 method_I <- function(X,k) {
   h <- sort(abs(grid-X))[k]
-  rejilla <- density(X, bw = h, kernel = "gaussian", from = -4, to = 4, n = n_rejilla)
+  rejilla <- density(X, bw = h, kernel = "gaussian", from = grid_min, to = grid_max, n = n_grid)
   return(rejilla)
   
 }
 
 # Ancho de banda Sheather-Jones y kernel gaussiano
 method_II <- function(X) {
-  rejilla <- density(X,bw="SJ", kernel = "gaussian", from = -4, to = 4, n = n_rejilla)
+  rejilla <- density(X, bw="SJ", kernel = "gaussian", from = grid_min, to = grid_max, n = n_grid)
   return(SJ)
 }
 
@@ -40,15 +41,15 @@ for (n in ns) {
     for (k in c(15,30,50)) {
       h_I <- method_I(X, k)
       # Guardamos los resultados
-      esta_vuelta <- data.frame(dato = h_I$y, metodo = rep(1,n_rejilla), tamaño = rep(n, n_rejilla), parametro = rep(k, n_rejilla))
+      esta_vuelta <- data.frame(dato = h_I$y, metodo = rep(1,n_grid), tamaño = rep(n, n_grid), parametro = rep(k, n_grid))
       results <- rbind(results, esta_vuelta)
     }
     
     
     # Calculamos la densidad estimada con el método II
-      h_II <- method_II(X,h)
+      h_II <- method_II(X)
       # Guardamos los resultados
-      esta_vuelta <- data.frame(dato = h_II$y, metodo = rep(2,n_rejilla), tamaño = rep(n, n_rejilla), parametro = rep(h, n_rejilla))
+      esta_vuelta <- data.frame(dato = h_II$y, metodo = rep(2,n_grid), tamaño = rep(n, n_grid), parametro = rep(h, n_grid))
       results <- rbind(results, esta_vuelta)
     
     cont=cont + 1
@@ -73,6 +74,10 @@ names(mse) <- c("metodo", "tamaño", "parametro", "ECM")
 min_mse_row <- mse[which.min(mse$ECM), ];min_mse_row
 
 solouno <- subset(results, metodo ==1 & tamaño ==50 & parametro == 50)
-plot(seq(-4,4, length = n_rejilla), solouno$dato, type = "l")
-curve(real_dens, from = -4, to = 4, add = TRUE)
+plot(seq(grid_min,grid_max, length = n_grid), solouno$dato, type = "l")
+curve(real_dens,grid_min,grid_max, add = TRUE)
 
+solouno <- subset(results, metodo ==2 & tamaño ==50 & parametro == 50)
+plot(seq(grid_min,grid_max, length = n_grid), solouno$dato, type = "l")
+curve(real_dens,grid_min,grid_max, add = TRUE)
+# Miro el de 15
